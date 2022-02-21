71bce64d17657117a5732157dc05e2cc80160efd
neskvikspivom <ilya.fedotko09@gmail.com>
2022-02-21 18:31:17 +0300

    neskvik s pivom)))))!!!!!11@!!@@!@!_@!_@___)(

--- routes.py
+++ routes.py
@@ -6,3 +6,11 @@ from app import app  # импортирую главный модуль сайт
 def main():  # вызовется функция
     return render_template('index.html')  # вернуть шаблон index.html
 
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
