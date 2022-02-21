c4c309721eae06fb2195ae715731eae8fdda4942
neskvikspivom <ilya.fedotko09@gmail.com>
2022-02-21 18:04:58 +0300

    neskvik s pivom)))))!!!!!11@!!@@!@!_@!_@___)(

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from routes import *  # подключаю файл с маршрутизацией к сайту
+from routes import *  # подключаю файл с маршрутизацией к  __name__ == '__main__':сайту
 
-if __name__ == '__main__':
+if
     app.run()
