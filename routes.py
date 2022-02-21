7cbd782cfa641a9f3abc348a2fd617c31483bb53
demidraksin <greatraksin@icloud.com>
2022-02-21 18:31:08 +0300

    Adding about us and contacts pages

--- routes.py
+++ routes.py
@@ -6,3 +6,13 @@ from app import app  # импортирую главный модуль сайт
 def main():  # вызовется функция
     return render_template('index.html')  # вернуть шаблон index.html
 
+
+@app.route('/contacts')  # если перейти на страницу contacts
+def contacts():
+    return render_template('contacts.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
